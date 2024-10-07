6d44c72f77c8d794663cdd2c1f828060a6cb9439
donglitiaoban <donglitiaoban@outlook.com>
2024-10-07 16:55:33 +0800

    set cursor style

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,26 +2,27 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
+config.default_cursor_style = 'SteadyBar'
 
 -- auto change colorscheme for light/dark mode
--- local function get_appearance()
---   if wezterm.gui then
---     return wezterm.gui.get_appearance()
---   end
---   return 'Dark'
--- end
--- 
--- local function scheme_for_appearance(appearance)
---   if appearance:find 'Dark' then
---     return 'Catppuccin Mocha'
---   else
---     return 'Catppuccin Latte'
---   end
--- end
--- 
--- config.color_scheme = scheme_for_appearance(get_appearance())
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
 
-config.color_scheme = 'Catppuccin Latte'
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Mocha'
+  else
+    return 'Catppuccin Latte'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+-- config.color_scheme = 'Catppuccin Latte'
 
 config.keys = {
   {
