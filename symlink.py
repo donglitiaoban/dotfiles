c6a661afa6af4a01b2284dfde089670da2ea06bb
donglitiaoban <donglitiaoban@outlook.com>
2024-10-10 14:52:36 +0800

    comment for why

--- symlink.py
+++ symlink.py
@@ -69,4 +69,6 @@ if __name__ == "__main__":
         target = home.joinpath(add_dot(filename))
         backup_replace(source, home.joinpath(".zshrc"))
     
+    # 因为python未识别导致脚本未运行，没有输出看上去就像正常执行
+    # 加一句print，看到就代表脚本已运行
     print("script executed")
