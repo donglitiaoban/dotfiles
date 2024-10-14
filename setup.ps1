49c626ccba29a1792084287454dfc807272e28e2
donglitiaoban <donglitiaoban@outlook.com>
2024-10-14 21:03:37 +0800

    todo

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,8 @@
 # todo: download and install vs buildtools
 # which is equivalent to xcode-select --install
 
+$url = "https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&channel=Release&version=VS2022&source=VSLandingPage&passive=false&cid=2030"
 if ($MyInvocation.CommandOrigin -eq 'Runspace') {
-  write-host 'ok'
+  $response = Invoke-WebRequest -Uri $url
+  Write-Host $response
 }
