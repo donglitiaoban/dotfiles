0952a65d01f220879cc0063ad6d41f9bcca111dd
donglitiaoban <donglitiaoban@outlook.com>
2025-01-23 14:42:43 +0800

    temp use dark mode

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -57,7 +57,7 @@ vim.g.transparent_enabled = true
 -- vim.g.zenbones_italic_comments = false
 
 vim.o.termguicolors = true
-vim.o.background = 'light'
+vim.o.background = 'dark'
 -- vim.cmd.colorscheme 'catppuccin'
 -- vim.cmd.colorscheme 'zenbones'
 -- vim.cmd.colorscheme 'deepwhite'
